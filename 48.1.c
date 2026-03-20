3b61af61103a7927310a5e77520b2f26b02ba7b3
Daksh <dakshgarg7777@gmail.com>
2026-03-20 22:53:50 +0530

    Create 48.1.c

---
+++ 48.1.c
@@ -0,0 +1,9 @@
+int countLeafNodes(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
