fc3bb874abe0cfa3e7331b2f3c29538244496387
Daksh <dakshgarg7777@gmail.com>
2026-04-13 12:06:09 +0530

    Create 62.1.c

---
+++ 62.1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int V;
+    struct Node** adj;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->adj = (struct Node**)malloc(V * sizeof(struct Node*));
+
+    for (int i = 0; i < V; i++)
+        graph->adj[i] = NULL;
+
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adj[src];
+    graph->adj[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adj[dest];
+    graph->adj[dest] = newNode;
+}
+
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->V; i++) {
+        struct Node* temp = graph->adj[i];
+        printf("%d: ", i);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    struct Graph* graph = createGraph(4);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+
+    printGraph(graph);
+
+    return 0;
+}
