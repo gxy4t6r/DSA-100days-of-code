f8f1ab229362a15aae674841a173585591819854
Daksh <dakshgarg7777@gmail.com>
2026-04-30 23:58:45 +0530

    Create 88.1.c

---
+++ 88.1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = 5;
+    int target = 7;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        printf("Found at index %d", result);
+    else
+        printf("Not found");
+
+    return 0;
+}
